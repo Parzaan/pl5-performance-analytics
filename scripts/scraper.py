7125b25a9af2d023053a6e20ff7bbf1a95454827
Parzaan <parz.aa.an@gmail.com>
2026-06-19 01:02:37 +0530

    Created scraper for player statistics and added gitignore

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,4 +1,19 @@
 import soccerdata as sd
-import pandas as pd
 import os
 
+os.makedirs("data/raw", exist_ok=True)
+
+fbref = sd.FBref(leagues="Big 5 European Leagues Combined",
+                 seasons=["2019-2020", "2020-2021", "2021-2022", "2022-2023", "2023-2024", "2024-2025"],
+                 no_cache=False
+                )
+
+df_standard_raw = fbref.read_player_season_stats(stat_type="standard")
+df_shooting_raw = fbref.read_player_season_stats(stat_type="shooting")
+df_playingtime_raw = fbref.read_player_season_stats(stat_type="playing_time")
+
+df_standard_raw.to_csv("data/raw/standard.csv")
+df_shooting_raw.to_csv("data/raw/shooting.csv")
+df_playingtime_raw.to_csv("data/raw/playingtime.csv")
+
+print("Scraping complete! CSV saved to data/raw/")
